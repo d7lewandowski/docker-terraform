3639388d84dd1430dfb28be2c463b02fcbbd3ee4
Damian Lewandowski <125447750+d7lewandowski@users.noreply.github.com>
2026-01-27 11:37:12 +0000

    tf-basic

---
+++ terraform/terraform_basic/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/workspaces/docker-terraform/terraform/dev/key_.json") 
+  project = "dtc-de-course-485115"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "data-lake-bucket_dtc_de_course_485115"
+  location      = "US"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "biqquery_dataset_dtc_de_course_485115"
+  project    = "dtc-de-course-485115"
+  location   = "US"
+}
